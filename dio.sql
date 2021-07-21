9f9a6b68d491fed3db5d8f568933f8716120c9dd
Ivisson Carlos Valverde Ferreira <ivisson.valverde@gmail.com>
2021-07-21 12:56:40 -0300

    Add files via upload

---
+++ dio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT, 
+    nome VARCHAR(30) NOT NULL,
+    nascimente DATE
+
+)
+ 
+ INSERT INTO pessoas (nome, nascimento) values ('Ivisson', '1980-01-31')
+ INSERT INTO pessoas (nome, nascimento) values ('Iasmin', '2013-10-16')
+ INSERT INTO pessoas (nome, nascimento) values ('Júlia', '2016-09-16')
